ed23c071b722e590811512bdfb6359db1b50852a
Roman Samoylin <roman.samoylin@gmail.com>
2024-11-07 11:10:05 +0300

    test speed

--- Spindel.java
+++ Spindel.java
@@ -22,6 +22,65 @@ public class SpindleSpeedAnalysis {
  }
 
  public void analyzeSpindleSpeeds() {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SpindleSpeedAnalysis {
+
+ private List<Double> spindleSpeeds;
+ private double[] coefficients;
+
+ public SpindleSpeedAnalysis(List<String> rawData) {
+ this.spindleSpeeds = new ArrayList<>();
+
+ for (String line : rawData) {
+ if (line.contains("PROCESS:1 SPINDLE SPEED:")) {
+ double spindleSpeed = Double.parseDouble(line.split(":")[1].trim());
+ this.spindleSpeeds.add(spindleSpeed);
+ }
+ }
+
+ this.coefficients = linearRegression(this.spindleSpeeds);
+ }
+
+ public double[] getCoefficients() {
+ return this.coefficients;
+ }
+
+ private double[] linearRegression(List<Double> spindleSpeeds) {
+ double mean = mean(spindleSpeeds);
+ double sumX = 0.0;
+ double sumY = 0.0;
+ double sumXY = 0.0;
+ double sumX2 = 0.0;
+ int n = spindleSpeeds.size();
+
+ for (int i = 0; i < n; i++) {
+ double x = i;
+ double y = spindleSpeeds.get(i) - mean;
+ sumX += x;
+ sumY += y;
+ sumXY += x * y;
+ sumX2 += x * x;
+ }
+
+ double slope = (n * sumXY - sumX * sumY) / (n * sumX2 - sumX * sumX);
+ double intercept = mean - slope * sumX / n;
+
+ double[] coefficients = {slope, intercept};
+ return coefficients;
+ }
+
+ private double mean(List<Double> numbers) {
+ double sum = 0.0;
+ for (double number : numbers) {
+ sum += number;
+ }
+ return sum / numbers.size();
+ }
+}
+
  // Здесь можно добавить код для анализа изменений скорости шпинделя
  // и построения графиков, например, с помощью библиотеки ChartJS
  }
