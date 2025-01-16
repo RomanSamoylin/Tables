6709a889cb423b2de6e13381be69486390aed017
Roman Samoylin <roman.samoylin@gmail.com>
2025-01-16 10:39:22 +0300

    parcing

---
+++ Parcing_onlain.py
@@ -0,0 +1,16 @@
+import pyodbc
+
+def get_data_from_ARMDMonitor():
+    connection_string = 'DRIVER={MySQL ODBC 8.0 Unicode Driver};SERVER=127.0.0.1;DATABASE=cnc_monitoring;UID=root;PWD=victoria123;PORT=3306'
+    conn = pyodbc.connect(connection_string)
+    cursor = conn.cursor()
+    query = "SELECT * FROM YOUR_TABLE_NAME"
+    cursor.execute(query)
+    data = cursor.fetchall()
+    conn.close()
+    return data
+
+if __name__ == "__main__":
+    data = get_data_from_ARMDMonitor()
+    for row in data:
+        print(row)
